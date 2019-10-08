52a58d417016bec42d05172300cbc86c78987ea1
mecki <mecki@fbb715cc-d8d7-4726-bffe-4b10094b3a44>
2019-10-08 22:24:10 +0000

    CheckStyle clean-up.

--- de.unkrig.zz/trunk/zz-pack/src/main/java/de/unkrig/zz/pack/Main.java
+++ de.unkrig.zz/trunk/zz-pack/src/main/java/de/unkrig/zz/pack/Main.java
@@ -66,7 +66,7 @@ class Main {
 
         this.pack.setExceptionHandler(new ExceptionHandler<IOException>() {
             @Override public void handle(String path, IOException ioe)     { Printers.error(path, ioe); }
-            @Override public void handle(String path, RuntimeException re) { Printers.error(path, re); }
+            @Override public void handle(String path, RuntimeException re) { Printers.error(path, re);  }
         });
     }
     private final LevelFilteredPrinter levelFilteredPrinter = new LevelFilteredPrinter();
